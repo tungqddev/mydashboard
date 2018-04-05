c5ffb2a328a5f5e0f8a3c9c3fd3e2767b3e9df38
TungQD <tungqd@topica.edu.vn>
2018-04-06 01:13:03 +0700

    angularfire simple chat demo login with google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
+import { Observable } from 'rxjs/Observable';
 import { Component } from '@angular/core';
-
+// import { AngularFire, AuthProviders, AuthMethods, FirebaseListObservable } from 'angularfire2'
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireList, } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app'
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  items:  AngularFireList<any>;
+  itemshow : Observable<any[]>;
+  name: any;
+  msgVal = '';
+  constructor(public af: AngularFireDatabase, public afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.items = af.list('messages',ref => ref.limitToLast(4));
+    afAuth.authState.subscribe((auth)=> {
+      this.name = auth;
+      this.itemshow = this.items.snapshotChanges().map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      });
+    });
+  }
+  login() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    // this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider());
+  }
+  chatsend(theirMessage: string) {
+    this.items.push({messages: theirMessage, name: this.name['displayName']});
+    this.msgVal='';
+  }
+  logout(){
+    this.afAuth.auth.signOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
+import { AngularFirestore } from 'angularfire2/firestore';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {AngularFireModule} from 'angularfire2';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
-
+//Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyCF2WNLWKyIFbP9miDGReji7CbLkc_787k",
+  authDomain: "mydashboard-5b31f.firebaseapp.com",
+  databaseURL: "https://mydashboard-5b31f.firebaseio.com",
+  projectId: "mydashboard-5b31f",
+  storageBucket: "mydashboard-5b31f.appspot.com",
+  messagingSenderId: "175656638845"
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase,
+    AngularFireAuth,
+    AngularFirestore
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
